56ccb10a6970f8544366f840a3a8193690a5b5df
parseoung <tpdud991209@naver.com>
2023-11-30 18:09:43 +0900

    2023.11.30

---
+++ chapter9.11/9.11.main.cpp
@@ -0,0 +1,98 @@
+#include <cassert>	
+#include <iostream>
+using namespace std;
+
+class Mystring
+{
+//private:
+public:
+	char* m_data = nullptr;
+	int m_length = 0;
+
+public:
+	Mystring(const char* source = "")
+	{
+		assert(source);
+
+		m_length = std::strlen(source) + 1;
+		m_data = new char[m_length];
+
+		for (int i = 0; i < m_length; ++i)
+			m_data[i] = source[i];
+
+		m_data[m_length - 1] = '\0';
+	}
+	
+	Mystring(const Mystring &source) // copy constructor
+	{
+		cout << "Copy constructor" << endl;
+		m_length = source.m_length;
+		
+		if (source.m_data != nullptr)
+		{
+			m_data = new char[m_length];
+
+			for (int i = 0; i < m_length; ++i)
+				m_data[i] = source.m_data[i];
+		}
+		else
+			m_data = nullptr;
+	}
+
+	Mystring& operator = (const Mystring& source)
+	{
+		/* shallow sopy
+		this->m_data = source.m_data;
+		this->m_length = source.m_length;
+		*/
+		cout << "Assignment operator" << endl;
+
+		if (this == &source) //prevent self-assignment
+			return *this;
+		
+		delete[] m_data;
+
+		m_length = source.m_length;
+
+		if (source.m_data != nullptr)
+		{
+			m_data = new char[m_length];
+
+			for (int i = 0; i < m_length; ++i)
+				m_data[i] = source.m_data[i];
+		}
+		else
+			m_data = nullptr;
+
+		return *this;
+	}
+
+	~Mystring()
+	{
+		delete[] m_data;
+	}
+
+	char* getString() { return m_data; }
+	int getlength() { return m_length; }
+};
+
+int main()
+{
+	Mystring hello("hello");
+
+	Mystring str1(hello); //copy constructor
+
+	Mystring str2; 
+	str2 = hello; // Assignment operator
+
+	cout << (int*)hello.m_data << endl;
+	cout << hello.getString() << endl;
+
+	{
+		Mystring copy = hello;
+		cout << (int*)copy.m_data << endl;
+		cout << copy.getString() << endl;
+	}
+
+	cout << hello.getString() << endl;
+}
